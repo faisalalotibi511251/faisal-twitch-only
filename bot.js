4fcc09b90911783da605473d52e5f7875dc0b2fb
faisalalotibi511251 <44735340+faisalalotibi511251@users.noreply.github.com>
2018-11-09 04:57:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : F9looh`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : F9looh ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`in Love ♥`,"http://twitch.tv/Straiming")
 client.user.setStatus("dnd")
 });
 
