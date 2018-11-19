c808c977174fa37cb2a2c28608fbc76d35a988a5
faisalalotibi511251 <44735340+faisalalotibi511251@users.noreply.github.com>
2018-11-19 22:57:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = '!'
 
 client.on('message', function(message) {
-    const myID = "321779633252990976";
+    const myID = "149511215264169984";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "changename")) {
                 if(message.author.id !== myID) return;
