9c834b3dfef7fb4fc6d4bf05d28762bb4c577ef9
faisalalotibi511251 <44735340+faisalalotibi511251@users.noreply.github.com>
2018-11-21 11:17:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,52 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
 
-client.on('message', function(message) {
-    const myID = "149511215264169984";
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "changename")) {
-                if(message.author.id !== myID) return;
-            if(!args) return message.reply('لا تلعب في حساب غيرك ياشاطر');
-        client.user.setUsername(args);
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "stream")) {
-                if(message.author.id !== myID) return;
-            if(!args) return message.reply('ا تلعب في حساب غيرك ياشاطر');
-        client.user.setGame(args , 'https://twitch.tv/6xlez1');
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "listen")) {
-                        if(message.author.id !== myID) return;
-            if(!args) return message.reply('ا تلعب في حساب غيرك ياشاطر');
-        client.user.setActivity(args, {type:'LISTENING'});
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-    });
+
+client.on('message', message => {
+    var prefix = "_";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("*bc <message>");
+    return;
     }
-});
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
